110fc1acf56b2c7ef9941bf65e9345f6efe868e0
rohankosti <kostirohan186@gmail.com>
2025-10-13 17:43:24 +0530

    updated with user input

--- Task_1/object_to_object.js
+++ Task_1/object_to_object.js
@@ -1,4 +1,6 @@
 const filesystem = require("fs");
+const { readFile } = require("node:fs/promises");
+const readline = require("node:readline/promises");
 const path = require("path");
 
 let filepath = path.join(__dirname, "product.json");
@@ -24,6 +26,39 @@ let newProduct = {
   },
 };
 
+let rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+async function new_obj() {
+  try {
+    let obj1 = await rl.question("Enter product name: ");
+    let obj2 = await rl.question("Enter product color: ");
+    let obj3 = await rl.question("Enter product price: ");
+
+    console.log(obj1);
+    console.log(obj2);
+    console.log(obj3);
+    rl.close();
+
+    newobj = {
+      data4: {
+        name: obj1,
+        color: obj2,
+        price: obj3,
+      },
+    };
+
+    const newobjread = await filesystem.promises.readFile(filepath, "utf-8");
+    // console.log(newobjread);
+    let newobjdata = JSON.parse(newobjread);
+    console.log(newobjdata);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 async function writeInitial() {
   try {
     await filesystem.promises.writeFile(
@@ -41,7 +76,7 @@ async function addNewProduct() {
   try {
     const response = await filesystem.promises.readFile(filepath, "utf-8");
     let data = JSON.parse(response);
-    const updatedData = { ...data, ...newProduct };
+    const updatedData = { ...data, ...newProduct, ...newobj };
     await filesystem.promises.writeFile(
       filepath,
       JSON.stringify(updatedData, null, 2),
@@ -54,7 +89,10 @@ async function addNewProduct() {
 }
 
 // First write initial products
-writeInitial().then(() => {
-  // Then add new product
-  addNewProduct();
+new_obj().
+then(() => {
+  writeInitial().
+  then(() => {
+    addNewProduct();
+  });
 });
