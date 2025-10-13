ecd8929a816af5472238048b71c9449d4a836926
rohankosti <kostirohan186@gmail.com>
2025-10-13 16:02:41 +0530

    lec_12 updated

---
+++ Lec_12/readline.js
@@ -0,0 +1,35 @@
+import readline from "node:readline/promises"; // return promise built in module ecma script module
+
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+// rl.question("what is your name? :- ", (name) => {
+//   console.log(`Hello ${name}`);
+  
+//   rl.question("What is your age:-", (age) => {
+//     console.log(`My age is ${age}`);
+//     rl.close();
+   
+//   });
+// });
+
+async function main() {
+
+  let data1= await rl.question("What is your Hobby :- ");
+   console.log(data1);
+
+  let data2= await rl.question("Kis color ki chaddi phene ho  :- ");
+   console.log(data2);
+   
+   rl.close();
+}
+
+main(); 
+
+
+
+
+
