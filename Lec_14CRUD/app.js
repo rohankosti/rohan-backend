bc56b89e9073f7ba75f73d0a49b116455b5bb2f6
rohankosti <kostirohan186@gmail.com>
2025-10-14 21:01:42 +0530

    Lec_14 update

--- Lec_14CRUD/app.js
+++ Lec_14CRUD/app.js
@@ -18,19 +18,41 @@ const server = createServer(async (req, res) => {
   try {
     if (req.method === "GET") {
       if (req.url === "/" || req.url === "/register.html") {
-        let data = await readFile(filepath, 'utf-8');
+        let data = await readFile(filepath, "utf-8");
         // console.log(data);
         res.writeHead(200, { "content-type": "text/html" });
         res.end(data);
-      } 
-      else if (req.url === "/register.css") {
+      } else if (req.url === "/register.css") {
         let csspath = path.join(dirname, "register.css");
         // console.log(csspath);
-        let read =await readFile(csspath,'utf-8');
+        let read = await readFile(csspath, "utf-8");
         // console.log(read);
-        res.writeHead(200,{"content-type":"text/css"});
+        res.writeHead(200, { "content-type": "text/css" });
         res.end(read);
+      } else if (req.url === "/login.html") {
+        let loginpath = path.join(dirname, "login.html");
+        let logpath = await readFile(loginpath, "utf-8");
+        console.log(logpath);
+        res.writeHead(200, { "content-type": "text/html" });
+        res.end(logpath);
+      } else if (req.url === "/login.css") {
+        let logincsspath = path.join(dirname, "login.css");
+        let logcsspath = await readFile(logincsspath, "utf-8");
+        console.log(logcsspath);
+        res.writeHead(200, { "content-type": "text/css" });
+        res.end(logcsspath);
       }
+    }
+
+    if (req.method === "POST" && req.url === "/RegisterData") {
+      let body = "";
+      req.on("data", (chunk) => {
+        body += chunk;
+        // console.log(body);
+        let urlpara = new URLSearchParams(body);
+        let object = Object.fromEntries(urlpara);
+        console.log(object);
+      });
     }
   } catch (error) {
     console.error(error);
