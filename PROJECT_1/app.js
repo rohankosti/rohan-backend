34f97d5b3d95d906288a3e16f7a6766267c40ca1
rohankosti <kostirohan186@gmail.com>
2025-10-27 16:43:09 +0530

    URL_SHORTNER

---
+++ PROJECT_1/app.js
@@ -0,0 +1,51 @@
+// Note:-
+// ✅ 1️⃣ FormData ek built-in JavaScript object hai
+// Jo HTML form ke andar ke saare input fields ka data collect karta hai.
+
+// ✅ 2️⃣ new FormData(form) —
+// Ye form element (HTML tag <form>...</form>) leta hai
+// aur uske andar ke har input ka name aur value pair store kar leta hai.
+// Example:
+// const form = document.getElementById("shortend_form");
+// const formData = new FormData(form);
+
+// ✅ 3️⃣ .get("name") —
+// Ye specific input field ka value deta hai
+// jiska name="..." attribute hota hai.
+
+let shortend_form = document.getElementById("shortend_form");
+console.log(shortend_form);
+
+shortend_form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const formdata = new FormData(shortend_form);
+//    const formdata = new FormData(e.target); optioninal 
+  console.log(formdata);
+
+  const url = formdata.get("url");
+  const shortcode = formdata.get("custom_url");
+
+  console.log(url, shortcode);
+});
+
+// const enter_url = document.getElementById("enter_url");
+// console.log(enter_url);
+
+// const custom_url = document.getElementById("custom_url");
+// console.log(custom_url);
+
+// const shortend_form = document.getElementById("shortend_form");
+// console.log(shortend_form);
+
+// shortend_form.addEventListener("submit",(e)=>{
+//  e.preventDefault();
+
+//    let data = enter_url.value;
+//    console.log(data);
+
+//    let url_data = custom_url.value;
+//    console.log(url_data);
+
+// });
+// old method
